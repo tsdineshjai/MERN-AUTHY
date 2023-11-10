c3a1a8d74d86e25113ca22d6fb1c5596090886dd
tanishka <tsdineshjai@gmail.com>
2023-11-10 21:31:13 +0530

    connected to mongoDB and established a connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+	.connect(
+		"mongodb+srv://tsdineshjai:AIeDCsrvhydUKBoR@mern-auth.kvn3ghi.mongodb.net/?retryWrites=true&w=majority"
+	)
+	.then(() => {
+		console.log(`database connection is successful`);
+	})
+	.catch((err) => console.log(err));
 
 const app = express();
 
 app.listen(3000, () => {
 	console.log(`Server listening on port 3000!!`);
 });
+
+console.log(process.env.HELLO);
