4655c5ec1046419d4edcb1d489a6364591be2555
tanishka <tsdineshjai@gmail.com>
2023-11-25 17:21:26 +0530

    User gets created in database

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,17 @@
-import User from "../models/user.modal";
+import User from "../models/user.modal.js";
+import bcryptjs from "bcryptjs";
 
-const signup = (req, res) => {
-  const { username, email, password } = req.body;
+const signup = async (req, res) => {
+	console.log(req.body);
+	const { username, email, password } = req.body;
+	const hashedPassword = bcryptjs.hashSync(password, 10);
+	const newUser = new User({ username, email, password: hashedPassword });
+	try {
+		await newUser.save();
+		res.status(201).json({ Status: "message succesfuly saved" });
+	} catch (error) {
+		res.status(500).json({ status: "duplicate username has been detected" });
+	}
 };
 
 export default signup;
